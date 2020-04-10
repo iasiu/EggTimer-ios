f1ad0856a168c13e1ecd198d9c0f77b163dcc821
Jan Lewandowski <iasiu@icloud.com>
2020-04-10 22:52:22 +0200

    added timer for buttons, progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var topLabel: UILabel!
     
-
+    // dict of cook times (min) * 60, depending on egg hardness
+    let cookTimes: [String: Int] = ["Soft": 5 * 60, "Medium": 7 * 60, "Hard": 12 * 60]
+    
+    // remaining seconds for egg to finish cooking
+    var secondsRemaining: Int = 0
+    
+    var timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // triggered when any button is pressed
+    @IBAction func buttonTouchUp(_ sender: UIButton) {
+        // reset timer
+        timer.invalidate()
+        
+        topLabel.text = "Wait for it..."
+        
+        // get time to cook an egg
+        secondsRemaining = cookTimes[sender.currentTitle!]!
+        
+        // tick timer every second
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsRemaining > 0 {
+                print("\(self.secondsRemaining) seconds.")
+                self.secondsRemaining -= 1
+            } else {
+                self.timer.invalidate()
+                self.topLabel.text = "DONE :)"
+            }
+        }
+    }
 
 }
